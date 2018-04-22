d3edc1503a41a16ef2122c53f85e6aa845b82706
Chansol <kkum04@hotmail.com>
2018-04-22 20:21:43 +0900

    제품 상세 페이지 완료
    Signed-off-by: Chansol <kkum04@hotmail.com>

---
+++ myapp/application/views/product/detail_v.php
@@ -0,0 +1,399 @@
+<!doctype html>
+
+<html>
+<head>
+    <title>이레스위치</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0">
+    <link href="/static/css/common.css" type="text/css" rel="stylesheet">
+    <link href="/static/css/main.css" type="text/css" rel="stylesheet">
+    <link href="/static/css/tab.css" type="text/css" rel="stylesheet">
+    <link href="/static/css/m.css" type="text/css" rel="stylesheet">
+    <link href="/static/css/sub.css" type="text/css" rel="stylesheet">
+    <link href="/static/css/sub_tab.css" type="text/css" rel="stylesheet">
+    <link href="/static/css/sub_m.css" type="text/css" rel="stylesheet">
+
+
+    <script src="/static/js/jquery-3.3.1.js"></script>
+    <script src="/static/js/common.js"></script>
+</head>
+
+<body>
+<div id="wrap">
+    <?php echo $this->header ?>
+    <!--sub_navi-->
+    <div id="sub_nav">
+        <div class="container">
+            <ul>
+                <li class="sub_nav_home">
+                    <a href="/"></a>
+                </li>
+                <li class="sub_nav_dp1 dp1_1">
+                    <a href="#">
+                        제품소개
+                    </a>
+                    <ul class="sub_nav_dp2 dp2_1">
+                        <li>
+                            <a href="/company/ceo">회사소개</a>
+                        </li>
+                        <li>
+                            <a href="/tech">기술현황</a>
+                        </li>
+                        <li>
+                            <a href="/customer">고객지원</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sub_nav_dp1 dp1_3">
+                    <a href="#">
+                        PULL CORD SWITCH
+                    </a>
+                    <ul class="sub_nav_dp2 dp2_2">
+                        <?php foreach($cur_category_products as $category_product): ?>
+                        <li>
+                            <a href="<?="/product/detail/{$category_product->product_id}"?>">
+                                <?=$category_product->product_name?>
+                            </a>
+                        </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!--//sub_navi-->
+
+    <section>
+        <article class="sub_bg" id="product_dt_pull">
+            <div class="container">
+                <h3 class="sub_title"><?=$product->category_name?></h3>
+                <div class="sub_content_box">
+                    <div class="prod_detail_wrap">
+                        <div class="prod_detail intro">
+                            <div class="intro_img">
+                                <img src="<?=$product->product_image?>" alt="제품 이미지"/>
+                            </div>
+
+                            <div class="intro_box">
+                                <dl class="intro_title">
+                                    <dt><?=$product->category_name?></dt>
+                                    <dd><?=$product->product_name.' '.$product->product_type?></dd>
+                                </dl>
+                                <div class="intro_btn">
+                                    <?php if($product->description_file): ?>
+                                    <a href="<?=$product->description_file?>" alt="제품설명서">
+                                        제품설명서
+                                    </a>
+                                    <?php endif;?>
+
+
+                                    <?php if($product->cad_file):?>
+                                    <a href="<?=$product->cad_file?>" alt="외형도">
+                                        외형도(Auto-CAD)
+                                    </a>
+                                    <?php endif;?>
+                                </div>
+
+                                <?php if($product->description != ''): ?>
+                                <div class="intro_txt">
+                                    <?=$product->description?>
+                                </div>
+                                <?php endif;?>
+                            </div>
+                        </div>
+
+                        <?php if($product->product_spec): ?>
+                        <div class="prod_detail spec">
+                            <h4><span>제품사양</span></h4>
+                            <ul>
+                                <?php foreach(explode("\n", $product->product_spec) as $line): ?>
+                                <li><?=$line?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                        <?php endif;?>
+
+                        <?php if($product->product_feature != ''): ?>
+                        <div class="prod_detail feat">
+                            <h4><span>제품특징</span></h4>
+                            <ul>
+                                <?php foreach(explode("\n", $product->product_feature) as $line): ?>
+                                    <li><?=$line?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                        <?php endif; ?>
+                    </div>
+                    <div class="btn_list">
+                        <a href="#">
+                            목록
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </section>
+
+
+    <footer>
+        <div class="container">
+            <div class="foot_col">
+                <img id="f_logo" src="images/common/logo2.png"/>
+                <p class="foot copy">COPTRIGHT ⓒ ERAESW ELECTRONICA CO.LTD. ALL RIGHTS RESERVED</p>
+            </div>
+            <div class="foot_col">
+                <h4 class="">공장</h4>
+                <adress>경남 김해시 상동면 동북로 473번길 278-30</adress>
+            </div>
+            <ul class="foot_col">
+                <li class="foot_info">TEL<span>055) 329 - 3855</span></li>
+                <li class="foot_info">FAX<span>055) 329 - 3856</span></li>
+                <li class="foot_info">E-mail<span>lsw6619@hanmail.net</span></li>
+            </ul>
+        </div>
+    </footer>
+
+    <!--사이트맵-->
+    <div class="pu pc_site_map">
+        <div class="pu_wrap">
+            <a class="close_btn" href="#">
+                <img src="images/common/close_btn.png">
+            </a>
+            <div class="pu_con">
+                <dl class="pu_title">
+                    <dt>사이트맵</dt>
+                    <dd>
+                        이레스위치의 모든 페이지를 한눈에 보실 수 있습니다.
+                    </dd>
+                </dl>
+                <ul class="site_map">
+                    <li class="site_map_top">
+                        <a href="#">회사소개</a>
+                        <ul class="site_map_sub">
+                            <li>
+                                <span>·</span>
+                                <a href="#">인사말</a>
+                            </li>
+                            <li>
+                                <span>·</span>
+                                <a href="#">회사연혁</a>
+                            </li>
+                            <li>
+                                <span>·</span>
+                                <a href="#">오시는 길</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="site_map_top">
+                        <a>제품소개</a>
+                        <div>
+                            <ul class="site_map_sub">
+                                <li>
+                                    <span>·</span>
+                                    <a href="#">PULL CORD SWITCH</a>
+                                    <ul class="site_map_prod">
+                                        <li>
+                                            <a href="#">LSW-101 일반형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-101L 일반램프형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-101R 일반형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-101RL 일반램프형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-101EX 방폭형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-101EXL 방폭램프형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-101EXR 방폭형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-101EXRL 방폭램프형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">MAGNET REED SWITCH(LSW-1012)</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">PULL CORD SWITCH ACCESSORY</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <span>·</span>
+                                    <a href="#">BELT ALIGNMENT SWITCH</a>
+                                    <ul class="site_map_prod">
+                                        <li>
+                                            <a href="#">LSW-102 일반형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-102EX 방폭형</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <span>·</span>
+                                    <a href="#">SPEED SWITCH</a>
+                                    <ul class="site_map_prod">
+                                        <li>
+                                            <a href="#">LSW-103EXA 분리방폭형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-103EXB 일체방폭형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-103EXC F.C WHEEL 방폭형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-103EXC URETHANE WHEEL형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-103M MAGNET WHEEL형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">SPEED SWITCH (ES-SM1) </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <span>·</span>
+                                    <a href="#">FLOW SWITCH</a>
+                                    <ul class="site_map_prod">
+                                        <li>
+                                            <a href="#">LSW-104EX 방폭형</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul class="site_map_sub">
+                                <li>
+                                    <span>·</span>
+                                    <a href="#">TILT(CHUTE) SWITCH</a>
+                                    <ul class="site_map_prod">
+                                        <li>
+                                            <a href="#">LSW-105C 일반형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-105EXA 방폭형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-105CM 경량형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-105AW</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-105DW</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-105TCS</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <span>·</span>
+                                    <a href="#">WARNING HORN</a>
+                                    <ul class="site_map_prod">
+                                        <li>
+                                            <a href="#">LSW-106EX</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-106EXL</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-106EXJ</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-106EXJL</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-106J2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-106JL2</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <span>·</span>
+                                    <a href="#">LIMIT SWITCH</a>
+                                    <ul class="site_map_prod">
+                                        <li>
+                                            <a href="#">LSW-107EXI</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW-107EXU</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <span>·</span>
+                                    <a href="#">ROPE SWITCH</a>
+                                    <ul class="site_map_prod">
+                                        <li>
+                                            <a href="#">LSW-108RW</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <span>·</span>
+                                    <a href="#">LEVEL SWITCH</a>
+                                    <ul class="site_map_prod">
+                                        <li>
+                                            <a href="#">LSW-111S 회전형</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">LSW109 정전용량형</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="site_map_top">
+                        <a href="#">기술현황</a>
+                        <ul class="site_map_sub">
+                            <li>
+                                <span>·</span>
+                                <a href="#">기술현황</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="site_map_top">
+                        <a href="#">고객지원</a>
+                        <ul class="site_map_sub">
+                            <li>
+                                <span>·</span>
+                                <a href="#">공지사항</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--//사이트맵-->
+
+</div>
+<script>
+    $( function () {
+        $('.exp_btn.prod_dt_exp').click( function () {
+            $('.pu.expend').fadeIn(300);
+        });
+
+        $('.pu,.close_btn').click( function () {
+            $('.pu.expend').fadeOut(300);
+        })
+    });
+</script>
+
+
+</body>
+</html>
