65746d3d626ee271a79a834d4789d92e46e5fa4e
Chansol <kkum04@hotmail.com>
2018-04-28 16:31:49 +0900

    sitemap 수정
    Signed-off-by: Chansol <kkum04@hotmail.com>

--- myapp/application/views/include/sitemap_v.php
+++ myapp/application/views/include/sitemap_v.php
@@ -11,6 +11,65 @@
                 </dd>
             </dl>
 
+            <ul class="site_map">
+                <li class="site_map_top">
+                    <a href="#">회사소개</a>
+                    <ul class="site_map_sub">
+                        <li>
+                            <span>·</span>
+                            <a href="/company/ceo">인사말</a>
+                        </li>
+                        <li>
+                            <span>·</span>
+                            <a href="/company/history">회사연혁</a>
+                        </li>
+                        <li>
+                            <span>·</span>
+                            <a href="/company/way">오시는 길</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="site_map_top">
+                    <a>제품소개</a>
+                    <ul class="site_map_sub">
+                        <?php foreach($categories as $category): ?>
+                            <li>
+                                <span>·</span>
+                                <a href="/category/<?=$category->category_id?>"><?=$category->category_name?></a>
+                                <ul class="site_map_prod">
+
+                                    <?php foreach($category->products as $product):?>
+                                        <li>
+                                            <a href="/product/detail/<?=$product->product_id?>">
+                                                <?=$product->product_name.' '.$product->product_type?>
+                                            </a>
+                                        </li>
+                                    <?php endforeach;?>
+
+                                </ul>
+                            </li>
+                        <?php endforeach;?>
+                    </ul>
+                </li>
+                <li class="site_map_top">
+                    <a href="/tech">기술현황</a>
+                    <ul class="site_map_sub">
+                        <li>
+                            <span>·</span>
+                            <a href="/tech">기술현황</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="site_map_top">
+                    <a href="/customer">고객지원</a>
+                    <ul class="site_map_sub">
+                        <li>
+                            <span>·</span>
+                            <a href="/customer">공지사항</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
