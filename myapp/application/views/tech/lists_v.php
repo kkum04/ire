fc2a16bcd8990240ce54fe546e24b08762298fba
thPark <thPark@imex.co.kr>
2018-05-12 12:27:05 +0900

    기술현황의 다운로드 버튼은 다운로드 되도록 수정
    Signed-off-by: thPark <thPark@imex.co.kr>

--- myapp/application/views/tech/lists_v.php
+++ myapp/application/views/tech/lists_v.php
@@ -75,7 +75,7 @@
                                         <dd><?=$tech->tech_number?></dd>
                                     </dl>
                                     <a class="exp_btn" href="#"></a>
-                                    <a class="dl_btn" href="<?=$tech->tech_image?>" target="_blank"></a>
+                                    <a class="dl_btn" href="<?=$tech->tech_image?>" target="_blank" download></a>
                                     <div class="pu expend">
                                         <div class="pu_wrap tech_wrap">
                                             <a class="close_btn" href="#">
